b9e9b75ea4e199d026230547853fb62a0033f00a
sandiren <sandiren@hotmail.com>
2025-09-28 16:04:46 +0400

    modified app.py

--- app.py
+++ app.py
@@ -457,8 +457,8 @@ def _generate_qr_png(data: str) -> BytesIO:
     return buffer
 
 
-with app.app_context():
-    db.create_all()
+#with app.app_context():
+    #db.create_all()
 
 
 if __name__ == "__main__":
